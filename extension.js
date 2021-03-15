839d6d3bf3b82bba8b872b450c2eca28c31a8cf7
Monu Kumar <monuk7735@gmail.com>
2021-03-15 12:56:51 +0530

    Just Username in TopBar:LeftPanel

--- extension.js
+++ extension.js
@@ -3,35 +3,59 @@ const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 class Extension {
-
   constructor() {
-    this.panelButton = new St.Bin({
+    // this.panelButton = new St.Bin({
+    //   style_class: "panel-button",
+    //   y_align: Clutter.ActorAlign.CENTER
+    // });
+    this.panelButton = new St.BoxLayout({
       y_align: Clutter.ActorAlign.CENTER,
-      style_class: "container",
+      style_class: "panel-button",
     });
 
     this.panelButtonText = new St.Label({
       text: GLib.get_user_name().toString(),
-      style_class: "panelText",
+      style_class: "panel-text",
       y_align: Clutter.ActorAlign.CENTER,
     });
-    this.panelButton.set_child(this.panelButtonText);
+
+    let gicon = Gio.icon_new_for_string(
+      "/var/lib/AccountsService/icons/" + GLib.get_user_name()
+    );
+
+    this.userAvatar = new St.Icon({ gicon, style_class: "user-avatar" });
+
+    // this.userAvatarContainer = new St.Bin({
+    //   y_align: Clutter.ActorAlign.CENTER,
+    //   style_class: "user-avatar-container",
+    // });
+
+    // this.userAvatar.set_icon_size(22);
+    // this.userAvatar.set
+
+    // this.userAvatarContainer.set_child(this.userAvatar);
+
+    this.panelButton.add_child(this.panelButtonText);
+    this.panelButton.add_child(this.userAvatar);
   }
 
   enable() {
     log("enabled");
-    Main.panel.statusArea[
-      "aggregateMenu"
-    ]._power.indicators.insert_child_at_index(this.panelButton, -1);
+    // Main.panel.statusArea[
+    //   "aggregateMenu"
+    // ]._power.indicators.insert_child_at_index(this.panelButton, -1);
+    Main.panel._rightBox.insert_child_at_index(this.panelButton, -1)
   }
 
   disable() {
     log("disabled");
-    Main.panel.statusArea["aggregateMenu"]._power.indicators.remove_child(
-      this.panelButton
-    );
+    // Main.panel.statusArea["aggregateMenu"]._power.indicators.remove_child(
+    //   this.panelButton
+    // );
+    Main.panel._rightBox.remove_child(this.panelButton)
   }
 }
 
